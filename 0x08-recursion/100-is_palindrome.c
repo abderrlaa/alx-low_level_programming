0e2bbc4b1cac8791e38c1ba84d05904a6d691558
abderrahim laazizi <abderr.laazizi@gmail.com>
2023-10-26 21:30:42 +0200

    #

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,56 @@
+#include"main.h"
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ *
+ * Return: 1 if it is, 0 it's not
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+	{
+		return (1);
+	}
+	return (strcompare(s, 0, (str_lenght(s))));
+}
+
+/**
+ * str_lenght - returns the length of a string
+ * @j: string to calculate the length of
+ *
+ * Return: length of the string
+ */
+
+int str_lenght(char *j)
+{
+	if (*j == '\0')
+	{
+		return (0);
+	}
+	return (1 + str_lenght(j + 1));
+
+}
+
+/**
+ * strcompare - checks the characters recursively for palindrome
+ * @i: string to check
+ * @b: iterator
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
+
+int strcompare(char *i, int b, int len)
+{
+	if (*(i + b) != *(i + len))
+	{
+		return (0);
+	}
+	else if (b >= len)
+	{
+		return (1);
+	}
+	return (strcompare(i, b + 1, len - 1));
+}
