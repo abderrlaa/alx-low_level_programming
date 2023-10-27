25643d3ccd21ce3067bc27bca9b3705d244c89b9
abderrahim laazizi <abderr.laazizi@gmail.com>
2023-10-28 00:01:27 +0200

    #

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - prints the name of the progarmme
+ * @argc: argument count
+ * @argv: arguments vector
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
