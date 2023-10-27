1c3b41934f0d375ff6220bdbdf3e39c81dfdad57
abderrahim laazizi <abderr.laazizi@gmail.com>
2023-10-28 00:13:57 +0200

    #

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - prints all arguments it receives
+ * @argc: argument count
+ * @argv: arguments vector
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	for (int i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
