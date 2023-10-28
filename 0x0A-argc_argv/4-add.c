a2940b3505b919388338cdc40e7294a96265d2d6
abderrahim laazizi <abderr.laazizi@gmail.com>
2023-10-28 13:23:34 +0200

    #

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+
+/**
+ * main -   that adds positive numbers, followed by a new line
+ * @argc: argument count
+ * @argv: arguments vector
+ * @i: iterator
+ * @sum: sum of numbers
+ * Return: 0 If no number is passed to the program,
+ * error if  one of the number contains symbols that are not digits,
+ *  sum if success
+*/
+int main(int argc, char *argv[])
+{
+	 int i, sum = 0;
+
+	if (argc == 1)
+	{
+		return (0);
+		printf("\n");
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(atoi(argv[i])))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		return (1);
+	}
+	return (sum);
+}
