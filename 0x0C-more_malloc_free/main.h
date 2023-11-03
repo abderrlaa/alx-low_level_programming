f3eff43afad213d10952e460bd47b04c341645b9
abderrahim laazizi <abderr.laazizi@gmail.com>
2023-11-03 21:45:30 +0100

    #

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,2 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+int *array_range(int min, int max);
 void *malloc_checked(unsigned int b);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif 
