88c47b2aadc682ffc758598b5b7db9d6d38bc4e6
abderrahim laazizi <abderr.laazizi@gmail.com>
2023-11-17 22:44:44 +0100

    #

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - multiplication of 2 numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	return (atoi(argv[1]) * atoi(argv[2]));
+}
