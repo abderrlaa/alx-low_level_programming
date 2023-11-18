1b8c13f2c92715d84deed7a9bf5586eab789677d
abderrahim laazizi <abderr.laazizi@gmail.com>
2023-11-18 16:36:52 +0100

    #

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+
+	for (i = 0; i < argc; i++)
+	{
+		for (j = 0; j != '\0'; i++)
+		{
+			if (!isdigit(atoi(argv[i][j])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+		}
+		sum += atoi(argv[i]);
+	}
+
+
+	return (0);
+}
