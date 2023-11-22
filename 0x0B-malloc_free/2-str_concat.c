9dad1f08ed93acbe98b02fb16ca4f03eda56962e
abderrahim laazizi <abderr.laazizi@gmail.com>
2023-11-22 20:40:22 +0100

    #

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: the the first string to copy from
+ * @s2: the the second string to copy from
+ * Return: pointer that points to a newly allocated space in memory
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	int strlen1 = 0;
+	int strlen2 = 0;
+	char *s;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		strlen1++;
+	}
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		strlen2++;
+	}
+
+	s = malloc(sizeof(char) * (strlen1 + strlen2) + 1);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		s[i] = s1[i];
+	}
+	for (i = 0; s2[i] = '\0'; i++)
+	{
+		s[strlen1 + i] = s2[i];
+	}
+	return (s);
+}
