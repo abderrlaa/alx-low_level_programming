94e60b41bc66425d2f10eff28d86dc0eb6dd4043
abderrahim laazizi <abderr.laazizi@gmail.com>
2023-11-22 20:45:52 +0100

    #

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,6 +14,15 @@ char *str_concat(char *s1, char *s2)
 	int strlen2 = 0;
 	char *s;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		strlen1++;
