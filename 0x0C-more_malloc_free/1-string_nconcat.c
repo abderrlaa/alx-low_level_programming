d1ae1ff51ce7f05b710561043476a48ec9aadd34
abderrahim laazizi <abderr.laazizi@gmail.com>
2023-11-30 10:51:44 +0100

    #

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,11 +13,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 	{
-		s1 = "";
+		s1len = 0;
 	}
 	if (s2 == NULL)
 	{
-		s2 = "";
+		s2len = 0;
 	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
